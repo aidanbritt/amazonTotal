cc468569710f0233e9523492da1184707e6613f1
aidanbritt <32249884+aidanbritt@users.noreply.github.com>
2018-05-15 22:31:19 -0400

    Using data-price attr & prettifying output
    - All Prices are now found even if they are new/used prices
    - Keeps track of items that are unavailable
    - Use 2 decimal places

--- main.js
+++ main.js
@@ -1,28 +1,37 @@
-var wholePrices = document.getElementsByClassName("a-price-whole");
-var fracPrices = document.getElementsByClassName("a-price-fraction");
-var wp = 0;
-var fp = 0;
+var prices = document.getElementsByClassName("g-item-sortable");
+var totalPrice = 0;
+var notFound = 0;
 
-wholePrices.length
-
-for(var i = 0; i < wholePrices.length; i++){
-    var dollars = parseInt(wholePrices[i].innerText);
-    var cents = parseInt(fracPrices[i].innerText);
-    if(!isNaN(dollars+cents)){
-        console.log("$"+wp+" + $"+dollars+"=");
-        wp += dollars;
-        console.log("$"+wp+"\n");
-        console.log(fp+"￠ +"+cents+"￠ =");
-        fp += cents;
-        console.log(fp+"￠  \n");
+for(var i = 0; i < prices.length; i++){
+    var currentPrice = prices[i].getAttribute("data-price");
+    console.log(currentPrice);
+    currentPrice = parseFloat(currentPrice);
+    console.log(currentPrice+"\n");
+    if(!isNaN(currentPrice) && currentPrice != Number.NEGATIVE_INFINITY){
+        console.log("$"+totalPrice+" + $"+currentPrice+"=");
+        totalPrice += currentPrice;
+        console.log("$"+totalPrice+"\n");
+    }else{
+        notFound++;
     }
 }
+var prettyPrice = totalPrice.toString();
+console.log(prettyPrice);
+var decimal = prettyPrice.indexOf(".");
 
-console.log("$"+wp+" + "+fp+"￠ =");
 
-wp += Math.floor(fp/100);
-fp %= 100;
+if(prettyPrice.length > decimal+3 && decimal!=-1){  //if price has trailing zeroes truncate
+    prettyPrice = prettyPrice.substr(0,decimal+3);  //ie 1.000000005 -> 1.00
+}
 
-console.log("$"+wp+"."+fp);
+else if(prettyPrice.length == decimal + 2){ //price ends in tenths place add zero
+    prettyPrice += "0";                     //ie (1.2) -> (1.20)
+}
 
-alert("$"+wp+"."+fp);
+var alertMsg = "Your Total is:\n$"+ prettyPrice;
+
+if(notFound > 0){
+    alertMsg += "\n\nPrices for "+notFound+" item(s) could not be found";
+}
+alert(alertMsg);
+console.log(prettyPrice);
